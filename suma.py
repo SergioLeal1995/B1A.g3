829bc0ab1b6010884a12a5626c51818bcc32fa81
Sergio Leal Capacho <serandreslealc95@gmail.com>
2019-09-24 06:23:15 -0500

    suma y resta

---
+++ suma.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+def suma(x,y):	#definicion de la función suma
+	print x+y
+
+#inicio del programa principal
+x = input("Ingrese el valor de x: ")
+y = input("Ingrese el valor de y: ")
+
+sumx(x,y)	#invocación de la función
