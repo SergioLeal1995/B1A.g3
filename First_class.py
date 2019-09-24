39b5fb87592d71e513ddce293340f8d103dbaada
Sergio Leal Capacho <serandreslealc95@gmail.com>
2019-09-23 21:06:53 -0500

    Evidencia primer taller python

--- First_class.py
+++ First_class.py
@@ -1,13 +1,21 @@
-import math
-import cmath
-import numpy as np
+# coding=utf-8
+import math #Libreria para usar pi
+import cmath #Libreria para números complejos
+import numpy as np #libreria para usar array's
 
-x_v = [9, 8, 7, 4, 5]
-y = 10
-x = 15
-y = math.cos(x)
-print("el resultado del coseno es: ", y)
+x_v1 = [9, 8, 7, 4, 5] # definición de una lista
+x_v = np.array(x_v1) # forma de convertir una lista en un array
+y = input("Ingrese el primer numero: ")
+x = input("Ingrese el segundo numero: ")
+#si utilizo cmath aparece parte compleja en los números!
+
+for i in range(5):
+	y = math.cos(x_v[i])
+	print "El valor para el coseno de ", x_v[i] , " es: ", y
+print ""	# Salto de línea                                             
+
+
 
 for i in range(5):
 	y_v = cmath.exp(2j*math.pi*x_v[i])
-	print (y_v)
+	print "El valor para la exponencial de ", x_v[i], "es: ", y_v
